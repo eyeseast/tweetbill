f67168707be31d468c0de76c784dff798422e523
Chris Amico <eyeseast@gmail.com>
2010-12-15 22:53:51 -0500

    It's NoArgs, not NoArg

--- tweetbill/apps/congress/management/commands/load_congress.py
+++ tweetbill/apps/congress/management/commands/load_congress.py
@@ -1,8 +1,8 @@
-from django.core.management.base import NoArgCommand
+from django.core.management.base import NoArgsCommand
 
 from congress import load
 
-class Command(NoArgCommand):
+class Command(NoArgsCommand):
     
     def handle_noargs(self, **options):
         "Just load all members"
